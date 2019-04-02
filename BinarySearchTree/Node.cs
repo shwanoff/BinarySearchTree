308ccb80d349d872a19dde0fe2133275b8719994
Вадим Шванов <adm.shwan@gmail.com>
2019-04-02 21:59:00 +0300

    Добавьте файлы проекта.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Node<T> : IComparable<T>, IComparable
+        where T: IComparable, IComparable<T>
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if(node.Data.CompareTo(Data) == -1)
+            {
+                if(Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if(Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Не совпадение типов");
+            }
+        }
+
+        public int CompareTo(T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.ReadLine();
+
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.ReadLine();
+
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
